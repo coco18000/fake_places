12eb479a9dc5f7da56285bec8c243ff2f8361809
Masahiro Nomura <masahironomura5325@gmail.com>
2015-12-02 22:35:15 +0900

    fix migration file

--- db/migrate/20151201142543_create_places.rb
+++ db/migrate/20151201142543_create_places.rb
@@ -1,7 +1,7 @@
 class CreatePlaces < ActiveRecord::Migration
   def change
     create_table :places do |t|
-      t.int :user_id, null:false
+      t.integer :user_id, null:false
       t.string :name, null: false
       t.float :longitude, null: false
       t.float :latitude, null: false
--- db/migrate/20151201142943_create_events.rb
+++ db/migrate/20151201142943_create_events.rb
@@ -1,8 +1,8 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-      t.int :user_id, null:false
-      t.int :place_id, null: false
+      t.integer :user_id, null:false
+      t.integer :place_id, null: false
       t.time :start_time, null: false
       t.time :end_time, null: false
 
